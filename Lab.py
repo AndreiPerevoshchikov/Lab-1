cd45e294c4805298645bc0c4c46fda889bf10fd1
AndreiPerevoshchikov <ap300304@yandex.ru>
2022-09-15 00:52:43 +0300

    Hello

--- Lab.py
+++ Lab.py
@@ -1,26 +1,50 @@
 import csv
-search = input('Поиск книги')
+#search = input('Поиск книги')
 f = open('result.txt', 'w')
-flag = 0
+#flag = 0
 with open('books.csv', 'r') as csvfile:
     books = csv.reader(csvfile, delimiter=';')
 # задание 1
-    print(len(list(books))-1)
-
+    z = -1
     title = 0
-    for row in list(books):
-        if len(row['Название']) > 30:
+    author = input('Введите желаемого автора: ')
+    search = input('Введите 1, если хотите ввести 20 ID книг для библиографической ссылки, иначе введите 0 для случайного набора ')
+    id_array = []
+    flag = 0
+    bible = 20
+    if search == '1':
+        for i in range(bible):
+            id_array.append(input('Введите ID искомой книги '))
+    else:
+        flag = 1
+
+
+    for row in books:
+        z += 1
+        if len(row[1]) > 30:
             title += 1
+
+        year = row[6].split()
+        year = year[0].split('.')
+        year = year[-1]
+
+        surname = row[3].split()
+        surname = surname[-1:]
+        if len(surname)>0:
+            if (author == str(surname[0])) and int(year) < 2016:
+                print(row[1])
+
+        if flag == 0:
+            if row[0] in id_array:
+                f.write('<' + str(surname[0]) + '>. <' + str(row[1]) + '> - <' + str(year) + '>' + '\n')
+        elif(flag == 1) and (z < 22) and (z > 1):
+            f.write('<' + str(surname[0]) + '>. <' + str(row[1]) + '> - <' + str(year) + '>' + '\n')
+
+
+
+    print(z)
     print(title)
+f.close()
 
-    for row in list(books):
-        lower_case = row[3, 4].lower()
-        index = lower_case.find(search.lower())
-        if (index != -1):
-            print(len(row[1]))
-            flag = 1
-            f.write(row[1] + '.' + row[3] + '. Стоимость ' + row[4] + 'рублей.\n')
 
-    if (flag == 0):
-        print('Nothing found')
 
